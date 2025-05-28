7743d05db1cf5e8bae5c533c3ffaeacb51979a50
W0LF12AM <bayui4742@gmail.com>
2025-05-28 23:05:35 +0700

    add bookmark logo, and learn js a bit

---
+++ JavaScript Basic/tugas1.js
@@ -0,0 +1,5 @@
+let saldo = prompt("Masukkan saldo Anda: ");
+let harga = 25000;
+let kembalian = saldo - harga;
+
+alert(`Saldo akhir anda adalah ${kembalian} dengan harga barang  ${harga}`);
---
+++ JavaScript Basic/tugas2.js
@@ -0,0 +1,5 @@
+let hari = new Date().getDay();
+let nama = ["minggu", "senin", "selasa", "rabu", "kamis", "jumat", "sabtu"];
+namaHari = nama[hari];
+
+alert(`Hari ini adalah hari ke-${hari} yaitu hari ${namaHari}`);
